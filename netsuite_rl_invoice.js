9e64392ef4acadf85519addf8c05563526781bce
Angel León <angi_lecas@hotmail.com>
2023-03-21 19:44:29 -0600

    Add files via upload

---
+++ netsuite_rl_invoice.js
@@ -0,0 +1,57 @@
+/**
+ * @author Angel Leon <angi.lecas93@gmail.com>
+ * @Name netsuite_rl_invoice.js
+ * @description Script que genera el número fibonacci para n y retorna el valor resultante
+ * @NApiVersion 2.1
+ * @NScriptType Restlet
+ */
+define(['N/log'], function (log) {
+   const entry_point = {
+      post: null
+   };
+
+   entry_point.post = function (request) {
+      log.audit('request', request);
+
+      var response = {
+         code: 200,
+         message: ""
+      };
+
+      try {
+         var n = parseInt(request.n);
+
+         if (isNaN(n)) {
+            response.code = 400;
+            response.message = "Valor inválido para n, debe ser númerico.";
+            return response;
+         }
+
+         var fibNum = getFib(n);
+
+         response.message = fibNum;
+      } catch (error) {
+         log.error('error', error);
+         response.code = 500;
+         response.message = error;
+      }
+
+      return response;
+   }
+
+   return entry_point;
+
+   /**
+    * Genera el número fibonacci para el indice n
+    * @param {Number} n Indice a generar
+    */
+   function getFib(n) {
+      var fibNum = [0, 1];
+
+      for (var i = 0; i < n; i++) {
+         fibNum.push(fibNum[i] + fibNum[i + 1]);
+      }
+
+      return fibNum[i];
+   }
+});
